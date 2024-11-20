27a3b860f8d110424fd37c1afc973651e04a5f62
unknown <saad123mn123@gmail.com>
2024-11-20 20:19:24 +0200

    update

--- app/[lng]/(website)/page.js
+++ app/[lng]/(website)/page.js
@@ -1,6 +1,7 @@
 import { getHeroImageUrls } from "@/actions/landing";
 import { EnhancedLandingPage } from "@/components/pages/enhanced-landing-page";
 
+export const dynamic = "force-dynamic";
 const HomePage = async ({ params: { lng } }) => {
   const heroImages = await getHeroImageUrls();
 
