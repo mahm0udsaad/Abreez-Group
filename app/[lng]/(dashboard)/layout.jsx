05853f8000e3ed87abdac5085fa4fa9ec84c001c
unknown <saad123mn123@gmail.com>
2025-01-15 17:05:33 +0200

    fix

--- app/[lng]/(dashboard)/layout.jsx
+++ app/[lng]/(dashboard)/layout.jsx
@@ -1,13 +1,17 @@
 import "@/app/[lng]/globals.css";
 import SideBar from "@/components/component/sidebar";
+import { enUS } from "@clerk/localizations";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default async function DashboardLayout({ params: { lng }, children }) {
   return (
-    <div className="flex h-screen bg-gray-900 text-gray-100">
-      {/* Sidebar */}
-      <SideBar lng={lng} />
-      {/* Main content */}
-      <main className="flex-1 overflow-y-auto p-8">{children}</main>
-    </div>
+    <ClerkProvider localization={enUS}>
+      <div className="flex h-screen bg-gray-900 text-gray-100">
+        {/* Sidebar */}
+        <SideBar lng={lng} />
+        {/* Main content */}
+        <main className="flex-1 overflow-y-auto p-8">{children}</main>
+      </div>
+    </ClerkProvider>
   );
 }
--- components/component/sidebar.jsx
+++ components/component/sidebar.jsx
@@ -14,6 +14,7 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { useTranslation } from "@/app/i18n/client";
+import { UserButton } from "@clerk/nextjs";
 
 export default function SideBar({ lng }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -24,80 +25,87 @@ export default function SideBar({ lng }) {
         isSidebarOpen ? "w-64" : "w-16"
       }`}
     >
-      <div className="p-4 flex justify-between items-center">
-        {isSidebarOpen && (
-          <h1 className="text-2xl font-bold text-blue-400 flex items-center">
-            <Image
-              src="/fav.jpg"
-              className="mx-2 h-6 w-6"
-              width={24}
-              height={24}
-              alt="logo"
-            />
-            {t("adminPanel")}
-          </h1>
-        )}
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="text-blue-400 hover:bg-gray-700"
-        >
-          {isSidebarOpen ? <ChevronLeft /> : <ChevronRight />}
-        </Button>
+      <div className="flex flex-col justify-between h-full">
+        <div className="">
+          <div className="p-4 flex justify-between items-center">
+            {isSidebarOpen && (
+              <h1 className="text-2xl font-bold text-blue-400 flex items-center">
+                <Image
+                  src="/fav.jpg"
+                  className="mx-2 h-6 w-6"
+                  width={24}
+                  height={24}
+                  alt="logo"
+                />
+                {t("adminPanel")}
+              </h1>
+            )}
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              className="text-blue-400 hover:bg-gray-700"
+            >
+              {isSidebarOpen ? <ChevronLeft /> : <ChevronRight />}
+            </Button>
+          </div>
+          <nav className="mt-6">
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-gray-200 hover:bg-gray-700"
+              asChild
+            >
+              <Link href="/dashboard">
+                <Package className="mr-2 h-4 w-4" />
+                {isSidebarOpen && t("products")}
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-gray-200 hover:bg-gray-700"
+              asChild
+            >
+              <Link href="/dashboard/add-product">
+                <Plus className="mr-2 h-4 w-4" />
+                {isSidebarOpen && t("addNewProduct")}
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-gray-200 hover:bg-gray-700"
+              asChild
+            >
+              <Link href="/dashboard/categories">
+                <Tag className="mr-2 h-4 w-4" />
+                {isSidebarOpen && t("categories")}
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-gray-200 hover:bg-gray-700"
+              asChild
+            >
+              <Link href="/dashboard/settings">
+                <Settings className="mr-2 h-4 w-4" />
+                {isSidebarOpen && t("settings")}
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-gray-200 hover:bg-gray-700"
+              asChild
+            >
+              <Link href="/dashboard/landing-page-manager">
+                <LayoutDashboard className="mr-2 h-4 w-4" />
+                {isSidebarOpen && t("landingPageManager")}
+              </Link>
+            </Button>
+          </nav>
+        </div>
+        <div className="p-4">
+          <UserButton />
+        </div>
       </div>
-      <nav className="mt-6">
-        <Button
-          variant="ghost"
-          className="w-full justify-start text-gray-200 hover:bg-gray-700"
-          asChild
-        >
-          <Link href="/dashboard">
-            <Package className="mr-2 h-4 w-4" />
-            {isSidebarOpen && t("products")}
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          className="w-full justify-start text-gray-200 hover:bg-gray-700"
-          asChild
-        >
-          <Link href="/dashboard/add-product">
-            <Plus className="mr-2 h-4 w-4" />
-            {isSidebarOpen && t("addNewProduct")}
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          className="w-full justify-start text-gray-200 hover:bg-gray-700"
-          asChild
-        >
-          <Link href="/dashboard/categories">
-            <Tag className="mr-2 h-4 w-4" />
-            {isSidebarOpen && t("categories")}
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          className="w-full justify-start text-gray-200 hover:bg-gray-700"
-          asChild
-        >
-          <Link href="/dashboard/settings">
-            <Settings className="mr-2 h-4 w-4" />
-            {isSidebarOpen && t("settings")}
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          className="w-full justify-start text-gray-200 hover:bg-gray-700"
-          asChild
-        >
-          <Link href="/dashboard/landing-page-manager">
-            <LayoutDashboard className="mr-2 h-4 w-4" />
-            {isSidebarOpen && t("landingPageManager")}
-          </Link>
-        </Button>
-      </nav>
     </aside>
   );
 }
