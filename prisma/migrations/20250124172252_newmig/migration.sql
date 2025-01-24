ae7b68586451ec2a587da8950788e164e1f0ca05
unknown <saad123mn123@gmail.com>
2025-01-24 19:23:00 +0200

    fix

---
+++ prisma/migrations/20250124172252_newmig/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `stockcoming` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "totalAvailable" INTEGER NOT NULL,
+    "multiImages" BOOLEAN NOT NULL DEFAULT false,
+    "categoryId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "itemSize" TEXT,
+    "itemWeight" TEXT,
+    "materials" TEXT,
+    "itemLocation" TEXT,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("categoryId", "createdAt", "description", "id", "itemLocation", "itemSize", "itemWeight", "materials", "multiImages", "name", "totalAvailable", "updatedAt") SELECT "categoryId", "createdAt", "description", "id", "itemLocation", "itemSize", "itemWeight", "materials", "multiImages", "name", "totalAvailable", "updatedAt" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+CREATE INDEX "Product_categoryId_idx" ON "Product"("categoryId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
